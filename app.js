89e5d0ffaabe344d7dd8808f808a2157fd73e1de
Simottardi <simottardi@gmail.com>
2021-09-05 19:17:52 +0200

    boilerplate complete

--- app.js
+++ app.js
@@ -3,8 +3,35 @@ app =express()
 
 require ('dotenv').config()
 
+const handlebars = require('express-handlebars')
+
+app.set('view engine','hbs')
+
+app.engine('hbs',handlebars({layoutsDir: `${__dirname}/views/layouts`, extname :'hbs',  defaultLayout: 'index', partialsDir: `${__dirname}/views/partials`}))
+
+app.use(express.static('public'))
+
+const num = () => {return [
+    {
+      name: 'one',
+    },
+    {
+      name: 'two',
+    },
+    {
+      name: 'three',
+    },
+    {
+      name: 'five',
+    },
+    {
+      name: 'seven',
+    }
+  ];
+}
+
 app.get('/', (req,res) => {
-    res.send('hello world!')
+    res.render('main',{layout:'index', theNumber: num(), listExists: true})
 })
 
 const PORT = process.env.PORT || 3000
